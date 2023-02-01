c85b16177808430731bb8a52165d5e1565a1bb97
jprgic <josip.prgic98@gmail.com>
2023-02-01 22:36:10 +0100

    add bubble sort and doc

--- sorting_algorithms/src/main.rs
+++ sorting_algorithms/src/main.rs
@@ -1,8 +1,11 @@
+// Using log and env_logger crates to log some info. Set env variable RUST_LOG=debug for
+// for more detailed output (on unix run export RUST_LOG=debug before running the app). 
+// Use log level info for less details
 use log::info;
 
 pub mod algorithms;
 
-use algorithms::{selection_sort, insertion_sort};
+use algorithms::{selection_sort, insertion_sort, bubble_sort};
 
 fn main() {
     env_logger::init();
@@ -10,6 +13,7 @@ fn main() {
     let v = vec![1, 32, -2, 23, 3, 5, 0, -111];
     info!("{:?}", selection_sort::sort(v));
     let v = vec![1, 32, -2, 23, 3, 5, 0, -111];
-    info!("{:?}", insertion_sort::sort(v))
-
+    info!("{:?}", insertion_sort::sort(v));
+    let v = vec![1, 32, -2, 23, 3, 5, 0, -111];
+    println!("{:?}", bubble_sort::sort(v));
 }
